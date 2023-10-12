78707735e8a7b539ab2dea587d639552044864c9
Namees Albayati <nameesmohammed12@gmail.com>
2023-10-12 09:36:17 -0500

    new

--- src0/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {createRoot} from 'react-dom'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
-
-
-const root=createRoot(document.getElementById('root'));
-root.render(<App/>)
